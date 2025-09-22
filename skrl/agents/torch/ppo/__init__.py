42a550574f1f9dd25547ccb2624117d59669242e
WSYANGSX <1062033103@qq.com>
2025-09-22 10:04:38 +0800

    modified

--- skrl/agents/torch/ppo/__init__.py
+++ skrl/agents/torch/ppo/__init__.py
@@ -1,2 +1,3 @@
 from skrl.agents.torch.ppo.ppo import PPO, PPO_DEFAULT_CONFIG
 from skrl.agents.torch.ppo.ppo_rnn import PPO_RNN
+from skrl.agents.torch.ppo.ppo_icm import PPO_ICM, PPO_ICM_DEFAULT_CONFIG
